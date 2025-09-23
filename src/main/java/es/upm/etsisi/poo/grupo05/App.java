97e8ae67aa5e24092367f2c895a398ebbbeea0b2
AdrianGarcia33 <adrian.garcia.soto@alumnos.upm.es>
2025-09-23 10:22:49 +0200

    Initial commit

---
+++ src/main/java/es/upm/etsisi/poo/grupo05/App.java
@@ -0,0 +1,10 @@
+package es.upm.etsisi.poo.grupo05;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
