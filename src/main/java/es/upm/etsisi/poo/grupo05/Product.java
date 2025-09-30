98d1cd13093d0905dc7a2d2d3b7e083f9c772911
Robert Corut <robertcorut04@gmail.com>
2025-09-30 09:58:49 +0200

    proucto acabasdo

--- src/main/java/es/upm/etsisi/poo/grupo05/Product.java
+++ src/main/java/es/upm/etsisi/poo/grupo05/Product.java
@@ -7,23 +7,95 @@ public class Product {
     Category category;
     private int quantity;
     boolean discount;
+    float discountPercentage;
 
 
-    public Product(int ID, String name, float price, Category category, int quantity){
+    public Product(int ID, String name, float price, Category category, int quantity) {
         this.ID = ID;
         this.name = name;
         this.price = price;
         this.category = category;
+        switch (category) {
+            case PAPELERIA:
+                this.discountPercentage = 0.95f;
+                break;
+            case ROPA:
+                this.discountPercentage = 0.93f;
+                break;
+            case LIBRO:
+                this.discountPercentage = 0.9f;
+                break;
+            case ELECTRONICA:
+                this.discountPercentage = 0.97f;
+                break;
+            default:
+                this.discountPercentage = 1f;
+                break;
+        }
     }
 
     public int getID() {
         return ID;
     }
+
     public String getName() {
         return name;
     }
-    public float getPrecio() {
+
+    public float getPrice() {
         return price;
     }
 
+    public Category getCategory() {
+        return category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean getDiscount() {
+        return discount;
+    }
+
+    public float getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setDiscount(boolean discount) {
+        this.discount = discount;
+    }
+
+    public void setDiscountPercentage(float discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+
+    public float getTotalPrice() {
+        float total = price * quantity;
+        if (discount == true) {
+            total *= discountPercentage;
+        }
+        return total;
+    }
 }
