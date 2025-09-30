29b5a207e99ca10362c6fe0bbbc80fa09a275865
AdrianGarcia33 <adrian.garcia.soto@alumnos.upm.es>
2025-09-30 10:27:52 +0200

    Receipt a medias

--- src/main/java/es/upm/etsisi/poo/grupo05/Receipt.java
+++ src/main/java/es/upm/etsisi/poo/grupo05/Receipt.java
@@ -4,26 +4,43 @@ import java.util.*;
 
 public class Receipt {
     private List<Product> ticket;
-    private int quantity;
+    private int numberItems;
+    private ProductList productList;
 
     public Receipt() {
         this.ticket = new LinkedList<>();
-        this.quantity = 0;
+        this.numberItems = 0;
     }
 
     public boolean reset(){
         this.ticket = new LinkedList<>();
-        this.quantity = 0;
+        this.numberItems = 0;
         return true;
     }
-    public boolean add(Product product, int quantity) {
-        boolean result = true;
-        if(product != null || quantity > 0){
-            for(Product p : ticket){
 
+    public boolean add(int id, int quantity) {
+        boolean result = false;
+        boolean added = false;
+        Product product = productList.getProduct(id);
+
+        // Si el producto ya esta
+        if (product != null && quantity > 0) {
+            for(Product p : ticket){
+                if (p.getID() == id) {
+                    p.setQuantity(p.getQuantity() + quantity);
+                    numberItems += quantity;
+                    result = true;
+                    added = true;
+                }
+            }
+            // Si no esta el producto
+            if (!added) {
+                product.setQuantity(quantity);
+                ticket.add(product);
+                numberItems += quantity;
+                result = true;
             }
         }
         return result;
-
     }
 }
