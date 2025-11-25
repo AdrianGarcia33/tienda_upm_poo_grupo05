23f064829ed6835ca2aaa0e63da177f6ad7f29f4
YujianChen_JiadongWang <YujianChen_JiadongWang@outlook.com>
2025-11-25 10:02:52 +0100

    Receipt

--- src/main/java/es/upm/etsisi/poo/grupo05/commandpackage/TicketCommands/Subcommands/TicketAddCommand.java
+++ src/main/java/es/upm/etsisi/poo/grupo05/commandpackage/TicketCommands/Subcommands/TicketAddCommand.java
@@ -1,7 +1,13 @@
 package es.upm.etsisi.poo.grupo05.commandpackage.TicketCommands.Subcommands;
 
+import es.upm.etsisi.poo.grupo05.ExceptionHandler;
 import es.upm.etsisi.poo.grupo05.resourcespackage.UserMap;
 import es.upm.etsisi.poo.grupo05.commandpackage.Command;
+import es.upm.etsisi.poo.grupo05.resourcespackage.userpackage.Cashier;
+import es.upm.etsisi.poo.grupo05.resourcespackage.userpackage.User;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TicketAddCommand extends Command {
     private UserMap userMap;
@@ -13,7 +19,46 @@ public class TicketAddCommand extends Command {
 
     @Override
     public boolean apply(String[] args) {
+        try{
+            String line = String.join(" ", args).trim();
+            Pattern p = Pattern.compile("^\\[(\\d+)\\]\\s*\"([^\"]+)\"\\s+(\\S+)\\s+(\\d+(?:\\.\\d+)?)\\s*(?:\\[(\\d+)\\])?$");
+            Matcher m = p.matcher(line);
 
+            if (!m.find()) {
+                return false; // formato no reconocido
+            }
+            String idStr = m.group(1);
+            String cashIdStr = m.group(2);
+            String prodIdStr = m.group(3);
+            String quantityStr = m.group(4);
+            String personalizedStr = m.group(5); //Este es opcional
+
+            int id = -1;
+            User cashier = null;
+            int prodId = -1;
+            int quantity = -1;
+
+            if (idStr != null) {
+                id = Integer.parseInt(idStr);
+            }
+            if (cashIdStr != null) {
+                cashier = userMap.getUserMap().get(cashIdStr);
+                if (!(cashier instanceof Cashier)) {
+                    System.out.println("No such cashier with ID: " + cashIdStr);
+                    return false;
+                }
+            }
+            if (prodIdStr != null) {
+                prodId = Integer.parseInt(prodIdStr);
+            }
+            if (quantityStr != null) {
+                quantity = Integer.parseInt(quantityStr);
+            }
+
+
+        }catch(IllegalArgumentException ex){
+            System.out.println(ExceptionHandler.getIllegalArgumentExceptionMessage());
+        }
 
 
 
