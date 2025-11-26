d7d3b277279f5a139f3c81e7192581335c37e3cf
YujianChen_JiadongWang <YujianChen_JiadongWang@outlook.com>
2025-11-26 14:14:09 +0100

    ReceiptCommand (sin terminar)

--- src/main/java/es/upm/etsisi/poo/grupo05/commandpackage/TicketCommands/Subcommands/TicketNewCommand.java
+++ src/main/java/es/upm/etsisi/poo/grupo05/commandpackage/TicketCommands/Subcommands/TicketNewCommand.java
@@ -1,7 +1,11 @@
 package es.upm.etsisi.poo.grupo05.commandpackage.TicketCommands.Subcommands;
 
+import es.upm.etsisi.poo.grupo05.ExceptionHandler;
 import es.upm.etsisi.poo.grupo05.resourcespackage.UserMap;
 import es.upm.etsisi.poo.grupo05.commandpackage.Command;
+import es.upm.etsisi.poo.grupo05.resourcespackage.receiptpackage.Receipt;
+import es.upm.etsisi.poo.grupo05.resourcespackage.userpackage.Cashier;
+import es.upm.etsisi.poo.grupo05.resourcespackage.userpackage.User;
 
 public class TicketNewCommand extends Command {
     private UserMap userMap;
@@ -12,7 +16,22 @@ public class TicketNewCommand extends Command {
 
     @Override
     public boolean apply(String[] args) {
-
+        try{
+            if (args.length == 2) {
+                String receiptId = null;
+                String cashId = args[0];
+                int userId = Integer.parseInt(args[1]);
+                User cashier = userMap.getUserMap().get(cashId);
+                cashier = userMap.getUserMap().get(cashId);
+                if (!(cashier instanceof Cashier)) {
+                    System.out.println("No such cashier with this ID ");
+                    return false;
+                }
+                cashier.getReceiptMap().newReceipt(new Receipt(null, cashId, userId));
+            }
+        }catch(IllegalArgumentException e){
+            System.out.println(ExceptionHandler.getIllegalArgumentExceptionMessage());
+        }
 
 
 
--- src/main/java/es/upm/etsisi/poo/grupo05/commandpackage/TicketCommands/Subcommands/TicketPrintCommand.java
+++ src/main/java/es/upm/etsisi/poo/grupo05/commandpackage/TicketCommands/Subcommands/TicketPrintCommand.java
@@ -2,6 +2,8 @@ package es.upm.etsisi.poo.grupo05.commandpackage.TicketCommands.Subcommands;
 
 import es.upm.etsisi.poo.grupo05.resourcespackage.UserMap;
 import es.upm.etsisi.poo.grupo05.commandpackage.Command;
+import es.upm.etsisi.poo.grupo05.resourcespackage.userpackage.Cashier;
+import es.upm.etsisi.poo.grupo05.resourcespackage.userpackage.User;
 
 public class TicketPrintCommand extends Command {
     private UserMap userMap;
@@ -12,15 +14,13 @@ public class TicketPrintCommand extends Command {
 
     @Override
     public boolean apply(String[] args) {
-
-
-
-
-
-
-
-
-
+        String receiptId = args[0];
+        User cashier = userMap.getUserMap().get(args[1]);
+        if (!(cashier instanceof Cashier)) {
+            System.out.println("No such cashier with this ID");
+            return false;
+        }
+        cashier.getReceiptMap().print(receiptId);
         return false;
     }
 }
