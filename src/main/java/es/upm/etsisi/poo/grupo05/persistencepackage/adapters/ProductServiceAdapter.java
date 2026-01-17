38132451af5b74cbf646fdd99b05ea90581098ac
Haoyu <haoyuc282@gmail.com>
2026-01-17 13:12:59 +0100

    Arreglado creo por fin

--- src/main/java/es/upm/etsisi/poo/grupo05/persistencepackage/adapters/ProductServiceAdapter.java
+++ src/main/java/es/upm/etsisi/poo/grupo05/persistencepackage/adapters/ProductServiceAdapter.java
@@ -1,13 +1,18 @@
 package es.upm.etsisi.poo.grupo05.persistencepackage.adapters;
 
 import com.google.gson.*;
+import es.upm.etsisi.poo.grupo05.persistencepackage.adapters.LocalDateAdapter;
+import es.upm.etsisi.poo.grupo05.persistencepackage.adapters.LocalDateTimeAdapter;
 import es.upm.etsisi.poo.grupo05.resourcespackage.productpackage.ProductService;
 import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ProductServiceAdapter implements JsonSerializer<ProductService>, JsonDeserializer<ProductService> {
 
     @Override
     public JsonElement serialize(ProductService src, Type typeOfSrc, JsonSerializationContext context) {
+        // Usamos el contexto para serializar, pero añadimos el type manualmente
         JsonObject jsonObject = context.serialize(src).getAsJsonObject();
         jsonObject.addProperty("type", "ProductService");
         return jsonObject;
@@ -15,8 +20,14 @@ public class ProductServiceAdapter implements JsonSerializer<ProductService>, Js
 
     @Override
     public ProductService deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        JsonObject jsonObject = json.getAsJsonObject();
+        // Para evitar el StackOverflow, NO usamos context.deserialize con la misma clase.
+        // Usamos un Gson auxiliar que sepa manejar fechas pero que NO tenga este adaptador registrado.
 
-        return context.deserialize(json, ProductService.class);
+        Gson tempGson = new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                .create();
+
+        return tempGson.fromJson(json, ProductService.class);
     }
 }
